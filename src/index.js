3c341c9352e768420ad637ebb6fdf1e515e96ff6
qilimazhualuo <49083614+qilimazhualuo@users.noreply.github.com>
2023-12-03 17:02:52 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import _ from 'lodash'
+import './index.css'
+
+function component() {
+    const element = document.createElement('div')
+
+    // lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+
+    return element
+}
+
+document.body.appendChild(component())
